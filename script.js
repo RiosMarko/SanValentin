86bdb6195fa20afbb1c69aa24258b185f7d9c9bf
Marco Rios <117676726+RiosMarko@users.noreply.github.com>
2025-02-11 21:27:04 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -18,3 +18,17 @@ document.getElementById("yes").addEventListener("click", function() {
     alert("Yay! 💖💖💖💖");
     alert("Te veo el 14 😉");
 });
+
+function createHeart() {
+    const heart = document.createElement('div');
+    heart.classList.add('heart');
+    heart.style.left = Math.random() * 100 + 'vw';
+    heart.style.animationDuration = (Math.random() * 2 + 3) + 's';
+    heart.style.zIndex = -1; 
+    document.body.appendChild(heart);
+    setTimeout(() => heart.remove(), 5000);
+}
+setInterval(createHeart, 200);
+
+
+
